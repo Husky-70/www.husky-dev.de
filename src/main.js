a453c64f6c48c3bfe6df96b3af04fa0c62e75521
Philipp <p.scheffler@ad-media.de>
2021-01-21 14:53:01 +0100

    fixed missing file

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import './plugins/axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify.js';
 import VueI18n from 'vue-i18n';
-import i18n from './i18n';
 require('./plugins/connector');
-import connector from './plugins/connector';
 
 Vue.use(VueI18n);
 
@@ -17,6 +15,5 @@ new Vue({
   router,
   store,
   vuetify,
-  i18n,
   render: h => h(App)
 }).$mount('#app');
